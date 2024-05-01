a216f39001ba3a86a9f18d1b30249dfc1a45d8cb
STALKSA <ksy9090@mail.ru>
2024-05-01 09:01:54 +0300

    added entity User and create table User

---
+++ src/main/java/org/klozevitz/First_REST_API/model/entities/User.java
@@ -0,0 +1,25 @@
+package org.klozevitz.First_REST_API.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
